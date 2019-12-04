60ab9b5dff012e7e67f881397e087863c2dd8e72
choibunbing <choibunbing@gmail.com>
2019-12-05 00:06:01 +0800

    Add：Nacos Demo

---
+++ java/src/main/java/spring/nacos/src/main/java/com/choibunbing/nacos/NacosApplication.java
@@ -0,0 +1,13 @@
+package com.choibunbing.nacos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NacosApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NacosApplication.class, args);
+    }
+
+}
---
+++ java/src/main/java/spring/nacos_consumer/src/test/java/com/choibunbing/nacos_consumer/NacosConsumerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.choibunbing.nacos_consumer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NacosConsumerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
